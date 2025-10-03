d212461c883b00b30521e16fb58ab95a5b9c7646
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-03 13:31:15 +0530

    message

---
+++ variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "Trump: Hello Putin, welcome to alaska"
+echo "Putin: Hello trump, thanks for inviting me"
+echo "Trump: How is the discussion with ukraine going on"
