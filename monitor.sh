e0faa13070a0ec1735faeed6c30aca689427b8b3
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-03 11:53:54 +0530

    message

--- monitor.sh
+++ monitor.sh
@@ -4,5 +4,8 @@ DISK=$(df -hT |grep -v  Filesystem)
 
 while IFS= read -r line
 do
-    DISK_USAGE=$(echo "$line" | awk '{print $6}')
+    DISK_USAGE=$(echo "$line" | awk '{print $6}'| cut -d "%" -f1)
+    PARTITION=$(echo "$line" | awk '{print $7}')
+    echo "print $PARTITION: $DISK_USAGE"
+
 done <<< $DISK
