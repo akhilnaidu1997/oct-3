c5ca65ccda793e28c45a9de9eadddca6e1872e86
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-03 15:52:09 +0530

    message

--- read.sh
+++ read.sh
@@ -4,3 +4,12 @@ echo "what is your username"
 read -s NAME
 
 echo "Print name: $NAME"
+
+echo "Please enter your password:"
+read -p "3 attempts left" password
+
+echo "print pawd:$password"
+
+echo "please enter again:"
+read -s -p "2 attempts left" passwd
+echo "print pwd: $passwd"
