790975b1a4797ac02dcfab2ea7b2c476adb280ee
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-03 13:47:41 +0530

    message

--- datatypes.sh
+++ datatypes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 LEADERS=("Modi" "Trump" "Putin" "JinPing")
-echo "Print all leaders $LEADERS[@]"
-echo "Print all leaders $LEADERS[0]"
-echo "Print all leaders $LEADERS[1]"
-echo "Print all leaders $LEADERS[2]"
+echo "Print all leaders ${LEADERS[@]}"
+echo "Print all leaders ${LEADERS[0]}"
+echo "Print all leaders ${LEADERS[1]}"
+echo "Print all leaders ${LEADERS[2]}"
