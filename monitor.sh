89a8da0e0de8cbcf539de3fe69f8d42a56499e06
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-03 11:55:18 +0530

    message

--- monitor.sh
+++ monitor.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 DISK=$(df -hT |grep -v  Filesystem)
+DISK_THRESHOLD=2 # usually 75 in orgs
 
 while IFS= read -r line
 do
     DISK_USAGE=$(echo "$line" | awk '{print $6}'| cut -d "%" -f1)
     PARTITION=$(echo "$line" | awk '{print $7}')
-    echo "print $PARTITION: $DISK_USAGE"
-
+    if [ $DISK_THRESHOLD -ge 2 ]; then
+        echo "print $PARTITION: $DISK_USAGE"
+    fi
 done <<< $DISK
