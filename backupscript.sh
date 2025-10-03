952cf3839e647a1c40af97f5fbd0227b6c91c554
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-03 09:04:52 +0530

    message

---
+++ backupscript.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USER=$(id -u)
+SOURCE=$1
+DEST=$2
+DAY=${3:-14}
+
+LOG_FOLDER="/var/log/shell-practice"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD 
+
+mkdir -p $LOG_FOLDER
+
+echo " Script started executing at : $(date)"
+
+if [ $USER -ne 0 ]; then
+    echo -e "Please proceed with $R sudo permissions $N"
+    exit 1
+fi
+USAGE(){
+    echo "USAGE: sudo sh filename.sh <Sourcedir> <Destdir> <Days> [optional, by default 14 days]"
+    exit 1
+}
+if [ $# -lt 2 ]; then
+    USAGE
+fi
+
+if [ ! -d $SOURCE ]; then
+    echo "ERROR: Source dir does not exist"
+    exit 1
+fi
+
+if [ ! -d $DEST ]; then
+    echo "ERROR: Dest doesnot exist"
+    exit 1
+fi
+
+FILES_TO_FIND=$(find $SOURCE -name "*.log" -type f -mtime +$DAY)
+
+if [ ! -z $FILES_TO_FIND ]; then
+    echo " Files are found in source"
+    TIME_STAMP=$(date +%F-%H-%M)
+    ZIPNAME=$($DEST/TIME_STAMP.zip)
+    echo "Zip file name: $ZIPNAME"
+else
+    echo "files didnot found"
+fi
