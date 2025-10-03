70a6e7dce341df9afc3c1b8e299a2e834198643a
Akhil Naidu <akhilnaid1997@gmail.com>
2025-10-03 16:21:51 +0530

    message

---
+++ special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All arguments passed to the script: $@"
+echo "All arguments passed to the script: $*"
+echo "script name: $0"
+echo "No of args passed to the script: $#"
+echo "user name: $USER"
+echo "current working directory: $PWD"
+echo "user home: $HOME"
+echo "process id of current script: $$"
+echo "Background script PID: $!"
+
